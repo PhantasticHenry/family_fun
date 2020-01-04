ed6867ba480da58d54b18c009b0ebf86dca3609c
github username <github email address>
2020-01-04 11:46:52 -0800

    add environment

---
+++ lib/environment.rb
@@ -0,0 +1,8 @@
+require_relative "./family_fun/version"
+require_relative "./family_fun/cli"
+require_relative "./family_fun/all_events"
+
+require "tty-prompt"
+require 'pry'
+require 'lolize'
+require 'colorize'
